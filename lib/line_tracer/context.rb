e06f07a1226fa769198f26d113fefa07e4cfe1a7
Corey Powell <mistdragon100@gmail.com>
2015-09-01 09:18:33 -0500

    Added more control over the frame timing
    `ghost_frames` are now provided on draw, along with `offset`, a `rate_mod` and `frame_mod`
    
    A rate_mod is a proc that returns a multiplier
    A frame_proc is a proc which wraps a normalized number, and must ALWAYS
    return between 0..1

--- lib/line_tracer/context.rb
+++ lib/line_tracer/context.rb
@@ -10,14 +10,14 @@ module LineTracer
     include MathUtils
 
     attr_reader :frames
+    attr_reader :frame_images
     attr_accessor :upscale
-    attr_accessor :ghost_frames
     attr_accessor :vert_prog
     attr_accessor :frag_prog
 
     def initialize(w, h, options = {})
       @frames = options.fetch(:frames, 64)
-      @ghost_frames = 1
+      @rendered_frames = options.fetch(:rendered_frames, @frames)
       @width, @height = w, h
       @upscale = 4
       @px_size = 1
@@ -25,7 +25,7 @@ module LineTracer
     end
 
     def create_frame_images
-      @frame_images = Array.new(@frames) { Minil::Image.create(@width, @height) }
+      @frame_images = Array.new(@rendered_frames) { Minil::Image.create(@width, @height) }
     end
 
     def make_frame_points(point_buffer)
@@ -39,29 +39,41 @@ module LineTracer
       end
     end
 
-    def draw(point_buffer, offset = 0)
+    def draw(point_buffer, **options)
+      offset = options.fetch(:offset, 0)
+      ghost_frames = options.fetch(:ghost_frames, 0)
+      rate_mod = options.fetch(:rate_mod) { ->(r) { r } }
+      frame_mod = options.fetch(:frame_mod) { ->(r) { r % 1 } }
+
       if !point_buffer.frame_points || point_buffer.frame_points.size != @frames
         point_buffer.frame_points = make_frame_points(point_buffer)
       end
 
-      step_per_frame = 1.0 / @frame_images.size.to_f
-      @frame_images.each_with_index do |img, i|
-        frame = i / @frame_images.size.to_f
-        (@ghost_frames).times do |j|
-          index = offset + i - j
+      ghost_frames = [ghost_frames, 1].max
 
-          r = ((offset + j) % @ghost_frames) / @ghost_frames.to_f
+      offset += point_buffer.offset
+      step_per_frame = 1.0 / @frames.to_f
+      @frame_images.each_with_index do |img, frame_index|
+        i = @frames * frame_index / @frame_images.size
+        i = rate_mod.call(i)
+
+        frame = frame_index / @frame_images.size.to_f
+
+        ghost_frames.times do |j|
+          index = offset + i - j
+          index = (frame_mod.call(index / @frames.to_f) * @frames).to_i
+
+          r = frame_mod.call((offset + j) / ghost_frames.to_f)
           norm = frame + r * step_per_frame
-          ghost_frame = (index % @frame_images.size) / @frame_images.size.to_f
+          ghost_frame = frame_mod.call(index / @frames.to_f)
+
+          timng = { frame: frame, ghost: r, norm: norm, ghost_frame: ghost_frame }
 
           pnt = get_point(point_buffer.frame_points, index)
           prev_p = prev_point(point_buffer.frame_points, index)
-          next unless pnt
-          next unless prev_p
-          timng = { frame: frame, ghost: r, norm: norm, ghost_frame: ghost_frame }
           pnt = @vert_prog.call(timng.merge(pos: pnt))
           prev_p = @vert_prog.call(timng.merge(pos: prev_p))
-          c = @frag_prog.call(timng)
+          c = @frag_prog.call(timng.merge(color: img.get_pixel(pnt[0], pnt[1])))
 
           draw_line prev_p, pnt do |x, y|
             img.fill_rect(x, y, @px_size, @px_size, c)
@@ -82,7 +94,7 @@ module LineTracer
         frame_names << filename
       end
 
-      puts "make_me_a_gif -f #{dirname}.gif #{frame_names.join(' ')}"
+      File.write('to_gif.sh', "#!/usr/bin/env bash\nmake_me_a_gif -d 1x60 -f #{dirname}.gif #{frame_names.join(' ')}")
     end
   end
 end
