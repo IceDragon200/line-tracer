d80441bb89492c0ce12ab4cb513ec23c2e5ed589
Corey Powell <mistdragon100@gmail.com>
2015-09-01 09:16:21 -0500

    PointBuffers now support offsets and options internally

--- lib/line_tracer/point_buffer.rb
+++ lib/line_tracer/point_buffer.rb
@@ -2,9 +2,13 @@ module LineTracer
   class PointBuffer
     attr_accessor :points
     attr_accessor :frame_points
+    attr_accessor :offset
+    attr_accessor :options
 
-    def initialize(points)
+    def initialize(points, **options)
       @points = points
+      @offset = options.delete(:offset) || 0
+      @options = options
     end
   end
 end
