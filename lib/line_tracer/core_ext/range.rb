6e946b685799e439fea881b6643b9bfdda07dd4c
Corey Powell <mistdragon100@gmail.com>
2015-09-13 12:01:29 -0500

    Added Range#diff

--- lib/line_tracer/core_ext/range.rb
+++ lib/line_tracer/core_ext/range.rb
@@ -1,5 +1,11 @@
 class Range
+  # @param [Integer] x  offset
+  # @return [Range]
   def translate(x)
     Range.new(x + self.begin, x + self.end, exclude_end?)
+  end
+
+  def diff
+    self.end - self.begin
   end
 end
